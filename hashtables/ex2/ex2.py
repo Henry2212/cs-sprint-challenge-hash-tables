3d138928515d94ac173a7e8bc118efe6467f634d
Henry <henrybsms@outlook.com>
2020-08-07 12:12:02 -0400

    1-3 solved

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -1,17 +1,53 @@
-def intersection(arrays):
+class Ticket:
+    def __init__(self, source, destination):
+        self.source = source
+        self.destination = destination
+
+# create cache
+cache = {}
+
+def reconstruct_trip(tickets, length):
     """
-    YOUR CODE HERE
+    Reconstructs a trip given a list of tickets.
+    Each tickect has a source and a destination.
+    The ticket for the first flight has source=None.
+    The ticket of the final flight has destination=None.
+    Returns:
+    -------
+    An array of strings with the route of the trip
     """
-    # Your code here
+    # create an empty list in which we will save the airports
+    route = []
 
-    return result
+    # hash each ticket such that source is the key and destination is value
+    for ticket in tickets:
+        cache[ticket.source] = ticket.destination
+    
+    # The first flight is the one that has source=None
+    route.append(cache['NONE'])
 
+    # add in route each flight
+    for idx in range(length):
+        # take the flight from cache
+        if route[idx] in cache:
+            # is it the first flight? If so, move on
+            if cache[route[idx]] == route[0]:
+                continue
+            # add the next destination
+            route.append(cache[route[idx]])
+    return route
 
-if __name__ == "__main__":
-    arrays = []
+tickets = [
+    Ticket("PIT", "ORD" ),
+    Ticket("XNA", "CID" ),
+    Ticket("SFO", "BHM" ),
+    Ticket("FLG", "XNA" ),
+    Ticket("NONE", "LAX"),
+    Ticket("LAX", "SFO" ),
+    Ticket("CID", "SLC" ),
+    Ticket("ORD", "NONE"),
+    Ticket("SLC", "PIT" ),
+    Ticket("BHM", "FLG" )
+]
 
-    arrays.append(list(range(1000000, 2000000)) + [1, 2, 3])
-    arrays.append(list(range(2000000, 3000000)) + [1, 2, 3])
-    arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
-
-    print(intersection(arrays))
+reconstruct_trip(tickets, 10)
